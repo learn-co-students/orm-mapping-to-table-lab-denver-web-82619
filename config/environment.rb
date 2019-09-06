37fd3e637fbe8516d770be6047c6e891c5d13327
github username <github email address>
2019-09-06 14:40:32 -0600

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
+require 'sqlite3'
+require 'pry'
 require 'bundler'
 Bundler.require
 
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+
+# binding.pry
